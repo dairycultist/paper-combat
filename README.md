101beb69e16a75ecfe181aecaed2878feabdffb4
dairycultist <kazumafan46@gmail.com>
2026-09-23 18:53:06 -0400

    Add wall edge, player plan, more references

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 retro filter, web game with preset levels
 
 https://fixvx.com/lemonadcake/status/2102746333472080110
+
+https://fixvx.com/FamicomCD/status/2102853675656065037
+
+https://fixvx.com/betta_daily/status/2102618350585049479
+
+35
---
+++ player.gd
@@ -0,0 +1,6 @@
+extends CharacterBody3D
+
+#A is red and B is blue btw
+#im gonna make a fighting game you press B to jump and A to punch and A+B to do a flying tornado kick and A in the air to do a flying kickdash and A then B to do an uppercut and A right on landing on the ground to do a sliding kickdash
+#the way im gonna implement all this is have timers representing the time since every single possible action
+#pressing A, B, also landing, and just a regular bool for the grounded state ig

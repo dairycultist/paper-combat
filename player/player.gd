b8033e7ac7d26adca3394e979b914f00708d5fa4
dairycultist <kazumafan46@gmail.com>
2026-09-24 08:45:59 -0400

    Full moveset

--- player/player.gd
+++ player/player.gd
@@ -12,6 +12,11 @@ var attack_t : float
 var landed_t : float
 var is_grounded : bool
 
+# TODO should probably bite the bullet and implement an attack state system
+# each attack state has conditions within which it can override the current
+# state (which includes which state is currently active). also movement
+# can depend on the state (i.e. run speed)
+
 func _process(delta: float) -> void:
 	
 	var anim_to_play : String
@@ -33,6 +38,7 @@ func _process(delta: float) -> void:
 		# just landed
 		if not is_grounded:
 			landed_t = 0.0
+			attack_t = 1000.0
 		
 		if Input.is_action_just_pressed("jump"):
 			jump_t = 0.0
@@ -50,21 +56,24 @@ func _process(delta: float) -> void:
 	
 	is_grounded = is_on_floor()
 	
-	if Input.is_action_just_pressed("attack") and attack_t > 0.35:
+	if Input.is_action_just_pressed("attack") and attack_t > 0.15:
 		attack_t = 0.0
 		
 		if not is_grounded:
 			velocity.x *= kickdash_speed_mult
 	
 	# determine attack
-	if attack_t < 0.3:
-		if abs(jump_t - attack_t) < 0.1 and not is_grounded:
-			anim_to_play = "flying_tornado_kick"
-		elif not is_grounded:
-			anim_to_play = "punch"
-		else:
-			anim_to_play = "punch"
-			velocity.x = lerp(velocity.x, 0.0, acceleration * 2.0 * delta)
+	if attack_t < 0.3 and abs(jump_t - attack_t) < 0.05 and not is_grounded:
+		anim_to_play = "flying_tornado_kick"
+	elif jump_t < 0.3 and attack_t - jump_t < 0.15:
+		anim_to_play = "uppercut"
+	elif attack_t < 0.3 and not is_grounded:
+		anim_to_play = "flying_kickdash"
+	elif attack_t < 0.3 and abs(landed_t - attack_t) < 0.1:
+		anim_to_play = "sliding_kickdash"
+	elif attack_t < 0.1:
+		anim_to_play = "punch"
+		velocity.x = lerp(velocity.x, 0.0, acceleration * 2.0 * delta)
 	
 	move_and_slide()
 	
